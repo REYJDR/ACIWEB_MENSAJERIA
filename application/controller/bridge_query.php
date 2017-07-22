279a705d9afa83197a4283f1bc13d8bf9c8ed0c5
REYJDR <REINALDO.DAOU@GMAIL.COM>
2017-07-22 14:02:25 -0500

    BQ
    #1

--- application/controller/bridge_query.php
+++ application/controller/bridge_query.php
@@ -470,7 +470,7 @@ public function get_msg_info($id){
 
   <legend>Detalle de envio</legend>
 
-  <table  id="table_info" width="100%" class="table table-striped table-bordered" cellspacing="0"  ><tbody>';
+  <table  id="table_info" width="100%" class="table table-striped table-bordered" cellspacing="0"  ><thead></thead><tbody>';
 
   foreach ($msg_detail as $datos) {
 
