e87c29454f8926b7bddeed6d19620faa139e3693
REYJDR <REINALDO.DAOU@GMAIL.COM>
2017-07-23 18:34:27 -0500

    update

--- application/view/home/dashboard.php
+++ application/view/home/dashboard.php
@@ -246,7 +246,8 @@ echo "<pre  id='code' class='prettyprint linenums'>
           labels: ['Sol. en transito asignadas']
       }).on('click', function(i, row){ 
 
-       table.fnFilter(row.z, 5; row.x, 3);
+       table.fnFilter(row.z, 5);
+       table.fnFilter(row.x, 3);
            
 
       });
